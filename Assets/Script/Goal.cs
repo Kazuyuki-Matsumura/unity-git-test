0e3d311ab113aa837cced90524feaf9ec7492b55
Kazuyuki-Matsumura <40016237+Kazuyuki-Matsumura@users.noreply.github.com>
2018-06-26 16:33:26 +0900

    ゴールを追加

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    public GameObject goal_window;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag =="Player")
+        {
+            goal_window.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
